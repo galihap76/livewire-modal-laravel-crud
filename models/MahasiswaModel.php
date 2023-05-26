5b3578d6a1d4a97a34d953daa546db617ce8954e
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2023-05-26 23:08:56 +0700

    Update MahasiswaModel.php

--- models/MahasiswaModel.php
+++ models/MahasiswaModel.php
@@ -10,9 +10,10 @@ class MahasiswaModel extends Model
     use HasFactory;
 
     protected $table = 'tbl_mahasiswa';
-    protected $primaryKey = 'nim';
+    protected $primaryKey = 'id_mahasiswa';
 
     protected $fillable = [
+        'nim',
         'nama',
         'jurusan'
     ];
